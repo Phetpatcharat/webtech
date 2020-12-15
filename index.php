017e49f1025743a2d33a32535c9175cb29c1dad4
Phetpatcharat <76037179+Phetpatcharat@users.noreply.github.com>
2020-12-15 16:04:13 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <h1>สวัสดี ศิลปากร</h1>
             <h2>Web Technology for Data Science</h2>
 
-            <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตร์บัณฑิต สาขาวิทยาการข้อมูล</p>
+            <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตร์บัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
             <p>ชื่อหลักสูตรภาษาอังกฤษ : Bachelor of Science Program in Data Science</p>
 
             <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตรสาขาวิทยาการข้อมูล</a>
